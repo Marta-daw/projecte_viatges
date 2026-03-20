83bcbee862694a6e4fc06b048384cf92dc2a5fa2
Marta Llabrés <marta.llab.daw@gmail.com>
2026-03-20 20:11:20 +0100

    new components

---
+++ resources/js/Components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import styles from './Hero.module.scss';
+
+export default function Hero() {
+    return (
+        <>
+            <div className={styles.heroContainer}>
+                <img srcSet="
+                    https://res.cloudinary.com/dadhzxpnj/image/upload/v1774030117/heroPhoto_skug90.jpg 400w,
+                    https://res.cloudinary.com/dadhzxpnj/image/upload/v1774030117/heroPhoto_skug90.jpg 800w,
+                    https://res.cloudinary.com/dadhzxpnj/image/upload/v1774030117/heroPhoto_skug90.jpg 1200w
+                " sizes="
+                    (max-width: 600px) 100vw,
+                    (max-width: 1024px) 50vw,
+                    (max-width: 1240px) 33vw,
+                    400px
+                " src="https://res.cloudinary.com/dadhzxpnj/image/upload/v1774030117/heroPhoto_skug90.jpg"
+                    alt="Imatge de viatge" width="1200" height="600" className={styles.heroImg} />
+            </div>
+            <div className={styles.heroTextContainer}>
+                <h1 >Benvingut a Destino Incierto</h1>
+                <p>
+                    Descobreix experiències úniques i viatges inoblidables.
+                </p>
+            </div>
+
+        </>
+
+    );
+}
