b3dd2ae0a64e7237122ca5912f6edeee80680379
Marta Llabrés <marta.llab.daw@gmail.com>
2026-03-23 17:45:55 +0100

    forms Login and register

--- resources/js/Components/AppLogo/AppLogo.jsx
+++ resources/js/Components/AppLogo/AppLogo.jsx
@@ -1,5 +1,6 @@
 import logo from '../../../images/LogoSinFondo.png';
 import styles from './AppLogo.module.scss';
+
 export default function ApplicationLogo(props) {
     return (
         <img src={logo} alt="Logo" className={styles.logo} />
---
+++ resources/js/Components/FormRegister/FormRegister.jsx
@@ -0,0 +1,24 @@
+import TextInput from '../TextInput';
+import styles from './FormRegister.module.scss'
+import PrimaryButton from '../PrimaryButton';
+
+export default function FormLogin() {
+    return (
+
+        <div className={styles.registerContainer}>
+            <h2 className={styles.registerTitle}>Registrar-se</h2>
+            <p>Nom</p>
+            <TextInput className={styles.inputLogin} />
+            <p>Email</p>
+            <TextInput className={styles.inputLogin} />
+            <p>contrasenya</p>
+            <TextInput className={styles.inputLogin} />
+            <div>
+                <PrimaryButton className={styles.buttonRegister}>
+                    Registra't
+                </PrimaryButton>
+            </div>
+        </div>
+
+    );
+}
