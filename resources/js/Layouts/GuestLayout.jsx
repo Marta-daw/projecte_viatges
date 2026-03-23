a49bdd89c7b4a50beb2633a129070a362b088ba1
Marta Llabrés <marta.llab.daw@gmail.com>
2026-03-23 17:45:55 +0100

    forms Login and register

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,12 +2,12 @@ import ApplicationLogo from '@/Components/AppLogo/AppLogo';
 import { Link } from '@inertiajs/react';
 
 import Header from '@/Components/Header/Header.jsx';
-import Hero from '@/Components/Hero/Hero';
+// import Hero from '@/Components/Hero/Hero';
 export default function GuestLayout({ children }) {
     return (
         <>
             <Header />
-            <Hero />
+            {/* <Hero /> */}
 
             <main>
                 <div >
