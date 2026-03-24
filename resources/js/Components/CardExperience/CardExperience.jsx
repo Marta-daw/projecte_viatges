16be19a14c0faf687925c7486d2bb87bbb8c88a4
Marta Llabrés <marta.llab.daw@gmail.com>
2026-03-24 17:04:59 +0100

    create AuthPanel component

--- resources/js/Components/CardExperience/CardExperience.jsx
+++ resources/js/Components/CardExperience/CardExperience.jsx
@@ -1,7 +1,9 @@
 import styles from './CardExperience.module.scss';
+import { Card } from "flowbite-react";
 
 function CardExperience({ experience }) {
-        return (
+    return (
+        <>
             <div className={styles.cardExperience}>
                 <div className={styles.imageContainer}>
                     <img src={experience.image_url} alt="Experience Image" className={styles.image} />
@@ -11,7 +13,24 @@ function CardExperience({ experience }) {
                     <p className={styles.description}>{experience.body}</p>
                 </div>
             </div>
-        );
+
+            <div className={styles.cardBody}>
+                <Card
+                    className={styles.card}
+                    imgAlt="experienceIMG"
+                    imgSrc={experience.image_url}
+                >
+
+                    <h5 className={styles.cardTitle}>
+                        {experience.title}
+                    </h5>
+                    <p className={styles.cardDescription}>
+                        {experience.body}
+                    </p>
+                </Card>
+            </div>
+        </>
+    );
 }
 
 export default CardExperience;
