810dcc074472dff90725883eded57b8f40aff8c7
Marta Llabrés <marta.llab.daw@gmail.com>
2026-04-07 17:01:58 +0200

    temp

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,15 @@ class DashboardController extends Controller
     public function index()
     {
         //Obtenim dades de la BBDD
-        $experiencies = Experiencia::latest()->take(3)->get();
+        $experiencies = Experiencia::query()
+            ->with(['user:id,name'])
+            ->withCount([
+                'votes as positive_votes_count' => fn ($q) => $q->where('value', 1),
+                'votes as negative_votes_count' => fn ($q) => $q->where('value', -1),
+            ])
+            ->latest()
+            ->get();
+
 
         //Renderitzem la pàgina de React i li passem les dades com a array
         return Inertia::render('Dashboard', [
