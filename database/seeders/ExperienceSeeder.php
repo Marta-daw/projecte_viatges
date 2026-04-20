15b793bb9affb77aff4b3d67506d37f1c5bc5502
Marta Llabrés <marta.llab.daw@gmail.com>
2026-04-20 17:14:18 +0200

    Update ExperienceSeeder.php

--- database/seeders/ExperienceSeeder.php
+++ database/seeders/ExperienceSeeder.php
@@ -21,11 +21,9 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 2,
                 'title' => 'Ruta pels Pirineus: Vall de Núria',
-                'body' => "**Una escapada inoblidable**.\n\n" .
-                    "Aquest cap de setmana hem fet una ruta impressionant per la **Vall de Núria**. Hem agafat el cremallera des de Ribes de Freser. \n\n" .
-                    "- Vistes espectaculars.\n\n" .
-                    "- Clima perfecte.\n\n" .
-                    'Recomano portar bon calçat i aigua.',
+                'body' => "Una escapada inoblidable. Aquest cap de setmana hem fet una ruta impressionant per la Vall de Núria, agafant 
+                    el cremallera des de Ribes de Freser. Les vistes han estat espectaculars i el clima, perfecte. Si hi voleu anar, us 
+                    recomano portar bon calçat i aigua.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093833/vall-de-nuria_arvzuf.avif',
                 'latitude' => 42.3961,
                 'longitude' => 2.1534,
@@ -37,10 +35,9 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 3,
                 'title' => 'Cap de setmana romàntic a Roma',
-                'body' => "**La Ciutat Eterna**\n\n" .
-                    "No hi ha res com passejar de nit pel centre de Roma. Vam visitar el Coliseu i vam tirar una moneda a la Fontana di Trevi.\n\n" .
-                    "_**Roma no es va fer en un dia, però es pot estimar en un segon.**_\n\n" .
-                    'Un viatge 10/10.',
+                'body' => "La Ciutat Eterna. No hi ha res com passejar de nit pel centre de Roma. Vam visitar el Coliseu i 
+                    vam tirar una moneda a la Fontana di Trevi. Un viatge 10/10.\n\n" .
+                    "Roma no es va fer en un dia, però es pot estimar en un segon.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093833/roma_spoanz.avif',
                 'latitude' => 41.8902,
                 'longitude' => 12.4922,
@@ -64,11 +61,10 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 2,
                 'title' => 'Descobrint Lisboa a peu',
-                'body' => "**La Ciutat dels Set Turons**\n\n" .
-                    "Lisboa és una ciutat que s\'ha de caminar sense pressa. Vam pujar al mirador de Santa Luzia al capvespre i la vista sobre el Tejo va ser increïble.\n\n" .
-                    "- Els elèctrics històrics són una experiència única.\n\n" .
-                    "- El pastís de nata de Belém és obligatori.\n\n" .
-                    'Imprescindible portar calçat còmode per als empedrats.',
+                'body' => "La Ciutat dels Set Turons. Lisboa és una ciutat que s'ha de caminar sense pressa. Vam pujar al 
+                    mirador de Santa Luzia al capvespre i la vista sobre el Tejo va ser increïble. Els elèctrics històrics són 
+                    una experiència única i el pastís de nata de Belém és obligatori. Imprescindible portar calçat còmode per als 
+                    empedrats.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093834/lisboa_bmipbj.avif',
                 'latitude' => 38.7169,
                 'longitude' => -9.1399,
@@ -80,11 +76,9 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 3,
                 'title' => 'Una setmana al nord de Marroc',
-                'body' => "**Xaouen, la Ciutat Blava**\n\n" .
-                    "Les medines blaves de Xaouen són com cap altre lloc del món. Ens vam perdre pels seus carrers estrets i vam trobar racons màgics a cada cantonada.\n\n" .
-                    "Viatjar és la única cosa que compres que et fa més ric.\n\n" .
-                    "- El soc és un caos meravellós ple de colors i olors.\n\n" .
-                    '- Recomanem allotjar-se a un riad local.',
+                'body' => "Xaouen, la Ciutat Blava.Les medines blaves de Xaouen són com cap altre lloc del món. Ens vam perdre 
+                pels seus carrers estrets i vam trobar racons màgics a cada cantonada. El soc és un caos meravellós ple de colors 
+                i olors, i recomanem allotjar-se en un riad local. Viatjar és l'única cosa que compres que et fa més ric.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093833/marroc_k8gkvk.avif',
                 'latitude' => 35.1688,
                 'longitude' => -5.2636,
@@ -96,12 +90,10 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 3,
                 'title' => 'Tokyo: entre tradició i modernitat',
-                'body' => "**El Japó que no oblides**\n\n" .
-                    "Arribar a Tokyo és com aterrar en un altre planeta. Vam visitar el temple de Senso-ji a l\'alba, abans que arribessin els turistes.\n\n" .
-                    "- El transport públic és impecable i puntual.\n\n" .
-                    "- El menjar al mercat de Tsukiji és una experiència única.\n\n" .
-                    "_**El Japó no és un destí, és una sensació.**_\n\n" .
-                    'Totalment recomanable per a viatgers curiosos.',
+                'body' => "El Japó que no oblides. Arribar a Tokyo és com aterrar en un altre planeta. Vam visitar el 
+                temple de Senso-ji a l'alba, abans que arribessin els turistes. El transport públic és impecable i
+                puntual, i el menjar al mercat de Tsukiji és una experiència única. Totalment recomanable per a viatgers curiosos.\n\n" .
+                "El Japó no és un destí, és una sensació.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093833/tokio_gzexqx.avif',
                 'latitude' => 35.7148,
                 'longitude' => 139.7967,
@@ -113,11 +105,9 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 2,
                 'title' => 'Escapada a la Costa Brava',
-                'body' => "**Cales i Tramuntana**\n\n" .
-                    "La Costa Brava a principis de temporada és un altre món. Sense masses, amb l\'aigua cristal·lina i els camins de ronda per a nosaltres sols. \n\n" .
-                    "- La cala de Tamariu és la més tranquil·la.\n\n" .
-                    "- El suro i la gastronomia local són imprescindibles.\n\n" .
-                    'Recomanem anar en temporada baixa per gaudir-ho de veritat.',
+                'body' => "Cales i Tramuntana. La Costa Brava a principis de temporada és un altre món: sense masses, amb 
+                    l'aigua cristal·lina i els camins de ronda per a nosaltres sols. La cala de Tamariu és la més tranquil·la, 
+                    i el suro i la gastronomia local són imprescindibles. Recomanem anar en temporada baixa per gaudir-ho de veritat.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093834/brava_qsrkju.avif',
                 'latitude' => 41.9109,
                 'longitude' => 3.2159,
@@ -141,12 +131,10 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 2,
                 'title' => 'Ruta per la Toscana en cotxe',
-                'body' => "**Entre vinyes i ciprers**\n\n" .
-                    "La Toscana és d\'aquells llocs que semblen una pintura a l\'oli. Vam recórrer els pobles medievals de Siena, San Gimignano i Montepulciano en tres dies.\n\n" .
-                    "- Les carreteres secundàries entre vinyes són espectaculars.\n\n" .
-                    "- Un bon chianti local és obligatori a cada àpat.\n\n" .
-                    "_**La Toscana et roba el cor sense demanar permís.**_\n\n" .
-                    'Recomanem llogar cotxe i allotjar-se en un agriturismo.',
+                'body' => "Entre vinyes i ciprers. La Toscana és d'aquells llocs que semblen una pintura a l'oli. Vam recórrer
+                    els pobles medievals de Siena, San Gimignano i Montepulciano en tres dies, per carreteres secundàries espectaculars 
+                    entre vinyes i un bon chianti local a cada àpat. Recomanem llogar cotxe i allotjar-se en un agriturismo. \n\n".
+                    "La Toscana et roba el cor sense demanar permís.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093833/toscana_bausht.avif',
                 'latitude' => 43.3186,
                 'longitude' => 11.3307,
@@ -158,12 +146,9 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 3,
                 'title' => 'Tres dies a Praga',
-                'body' => "**La Ciutat de les Cent Torres**\n\n" .
-                    "Praga és una de les ciutats més ben conservades d\'Europa. El barri jueu, el castell i el pont de Carles van ser els nostres favorits.\n\n" .
-                    "- Millor evitar el centre en cap de setmana: és ple de turistes.\n\n" .
-                    "- La cervesa txeca és la millor del món, i és baratíssima.\n\n" .
-                    "- El mercat de Nadal a la plaça de la Ciutat Vella és màgic.\n\n" .
-                    'Un viatge molt recomanable i assequible.',
+                'body' => "La Ciutat de les Cent Torres. Praga és una de les ciutats més ben conservades d'Europa. El barri 
+                    jueu, el castell i el pont de Carles van ser els nostres favorits. Millor evitar el centre en cap de setmana, 
+                    però la cervesa txeca és la millor del món i és baratíssima. Un viatge molt recomanable i assequible.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093834/praga_bpodpn.avif',
                 'latitude' => 50.0755,
                 'longitude' => 14.4378,
@@ -175,13 +160,10 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 2,
                 'title' => 'Aventura al Parc Nacional de Doñana',
-                'body' => "**Natura salvatge al sud d\'Espanya**\n\n" .
-                    "Doñana és un dels espais naturals més importants d\'Europa i és a tocar de casa. Vam fer una excursió guiada en tot terreny 
-            per veure linx ibèric i àguila imperial. \n\n" .
-                    "- És imprescindible reservar l\'excursió amb antelació.\n\n" .
-                    "- L\'alba al parc, amb la boira sobre les marismes, és inoblidable.\n\n" .
-                    "_**La naturalesa no necessita nosaltres, nosaltres la necessitem a ella.**_\n\n" .
-                    'Perfecte per a amants de la fauna i el birdwatching.',
+                'body' => "Natura salvatge al sud d'Espanya. Doñana és un dels espais naturals més importants d'Europa i és 
+                    a tocar de casa. Vam fer una excursió guiada en tot terreny per veure linx ibèric i àguila imperial: cal 
+                    reservar amb antelació. L'alba al parc, amb la boira sobre les marismes, és inoblidable. Perfecte per a amants de la fauna i el birdwatching.\n\n".
+                    "La naturalesa no necessita nosaltres, nosaltres la necessitem a ella.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093834/donana_ui2uvj.avif',
                 'latitude' => 36.9981,
                 'longitude' => -6.3362,
@@ -193,11 +175,9 @@ class ExperienceSeeder extends Seeder
             [
                 'user_id' => 3,
                 'title' => 'Cap de setmana a Amsterdam',
-                'body' => "**Canals, bicicletes i museus**\n\n" .
-                    "Amsterdam és una ciutat que es viu millor en bicicleta.\n\n" .
-                    "- Llogar una bici és la millor decisió.\n\n" .
-                    "- El mercat d'Albert Cuyp és ideal per esmorzar.\n\n" .
-                    'Una ciutat encantadora.',
+                'body' => "Canals, bicicletes i museus. Amsterdam és una ciutat que es viu millor en bicicleta: llogar-ne una és la 
+                    millor decisió. El mercat d'Albert Cuyp és ideal per esmorzar i cada canal amaga un racó encantador. 
+                    Una ciutat que no defrauda.",
                 'image_url' => 'https://res.cloudinary.com/dadhzxpnj/image/upload/q_auto/f_auto/v1776093834/amsterdam.jpg_lh1nz4.avif',
                 'latitude' => 52.3676,
                 'longitude' => 4.9041,
